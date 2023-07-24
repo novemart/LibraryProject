5da0e13fa7e03bd3b8d90480626bf6d056ac2129
Martina Yusuf <martina.yusuf2@gmail.com>
2023-07-24 17:33:25 +0200

    repo test disabled

--- src/test/java/com/example/LibrarySpringBoot/repository/MemberRepositoryTest.java
+++ src/test/java/com/example/LibrarySpringBoot/repository/MemberRepositoryTest.java
@@ -21,6 +21,7 @@ public class MemberRepositoryTest {
     MemberRepository memberRepo;
 
     @Test
+    @Disabled
     public void findMembersByFirstName(){
         //arrange
         Member m = new Member();
